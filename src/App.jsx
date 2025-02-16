ebfa7c9528d1b434cd2f0e252e98a55988324c68
enzoherce <vherce@purdue.edu>
2025-02-15 20:21:40 -0500

    Lab 10

--- src/App.jsx
+++ src/App.jsx
@@ -7,68 +7,11 @@ import Card from "./components/Card";
 import Wrapper from "./components/Wrapper";
 import { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faXmark, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import ProfileForm from "./components/ProfileForm";
 import { useEffect } from "react";
-import { use } from "react";
-
 
 const App = () => {
-  // const profiles = [
-  //   {
-  //     img: image_man,
-  //     name: "John Doe",
-  //     title: "Software Engineer",
-  //     email: "a@a.com",
-  //   },
-  //   {
-  //     img: image_woman,
-  //     name: "Lily Smith",
-  //     title: "UX designer",
-  //     email: "b@a.com",
-  //   },
-  //   {
-  //     img: image_man,
-  //     name: "Bob Johnson",
-  //     title: "Web developer",
-  //     email: "c@a.com",
-  //   },
-  //   {
-  //     img: image_woman,
-  //     name: "Ava Smith",
-  //     title: "Web developer",
-  //     email: "d@a.com",
-  //   },
-  //   {
-  //     img: image_man,
-  //     name: "Tom Smith",
-  //     title: "Software Engineer",
-  //     email: "e@a.com",
-  //   },
-  //   {
-  //     img: image_woman,
-  //     name: "Eva Smith",
-  //     title: "Graphic designer",
-  //     email: "f@a.com",
-  //   },
-  // ];
-
-  //Variable to store the animation state
-  const [profiles, setProfiles] = useState([]);
-  useEffect(() => {
-    fetch("https://web.ics.purdue.edu/~vherce/fetch-data.php")
-      .then((res) => res.json())
-      .then((data) => {
-        setProfiles(data);
-        console.log(data)
-      })
-  }, []);
-
-  const [animation, setAnimation] = useState(false);
-  //function to update the animation state
-  const handleAnimation = () => {
-    setAnimation(false);
-  };
 
   //Variable to store the mode state
   const [mode, setMode] = useState("light");
@@ -78,35 +21,48 @@ const App = () => {
   };
 
   // get titles
-  const titles = [...new Set(profiles.map((profile) => profile.title))];
+  const [titles, setTitles] = useState([]);
+  useEffect(() => {
+    fetch("https://web.ics.purdue.edu/~vherce/get-titles.php")
+    .then((res) => res.json())
+    .then((data) => {
+      setTitles(data.titles)
+    })
+  }, [])
 
   const [title, setTitle] = useState("");
   //update the title on change of the drowndrop
   const handleTitleChange = (event) => {
     setTitle(event.target.value);
-    setAnimation(true);
+    setPage(1);
   };
 
   const [search, setSearch] = useState("");
   //update the search on change of the input
   const handleSearchChange = (event) => {
     setSearch(event.target.value);
-    setAnimation(true);
+    setPage(1);
   };
+  const [profiles, setProfiles] = useState([]);
+  const [page, setPage] = useState(1);
+  const [count, setCount] = useState(1);
 
+  useEffect(() => {
+    fetch(`https://web.ics.purdue.edu/~vherce/fetch-data-with-filter.php?title=${title}&name=${search}&page=${page}&limit=10`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProfiles(data.profiles);
+        setCount(data.count);
+        setPage(data.page);
+      })
+  }, [title,search, page]);
   //clear the title and search
   const handleClear = () => {
     setTitle("");
     setSearch("");
-    setAnimation(true);
+    setPage(1);
   };
 
-  //filter the profiles based on the title
-  const filtedProfiles = profiles.filter(
-    (profile) =>
-     (title === "" || profile.title === title) &&
-      profile.name.toLowerCase().includes(search.toLowerCase())
-  );
   const buttonStyle = {
     border: "1px solid #ccc",
   };
@@ -158,15 +114,30 @@ const App = () => {
             </button>
           </div>
           <div className="profile-cards">
-            {filtedProfiles.map((profile) => (
+            {profiles.map((profile) => (
               <Card
                 key={profile.id}
                 {...profile}
-                animate={animation}
-                updateAnimate={handleAnimation}
               />
             ))}
           </div>
+          {
+            count === 0 && <p>No profiles found!</p>
+          }
+          {count > 10 &&
+          <div className="pagination">
+            <button onClick={() => setPage(page - 1)} disabled={page === 1}>
+              <span className="sr-only">Previous</span>
+              <FontAwesomeIcon icon={faChevronLeft} />
+              </button>
+            <span>{page}/{Math.ceil(count/10)}</span>
+            <button onClick={() => setPage(page + 1)} disabled={page >= Math.ceil(count/10)}>
+              <span className="sr-only">Next</span>
+              <FontAwesomeIcon icon={faChevronRight} />
+            </button>
+          </div>
+          }
+
         </Wrapper>
       </main>
     </>
